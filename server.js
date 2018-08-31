e56b4fe49792746db8f7e7d8feb5fa86e0557e20
Anna Machała <neskaa0@gmail.com>
2018-08-31 23:15:35 +0200

    textv4

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static('./dist/angularr-test-app'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join('/dist/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/angularr-test-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log('slucham');
