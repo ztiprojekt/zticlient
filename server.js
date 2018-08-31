e9750022837a27cb428a7d51f0b012555898f408
Anna Machała <neskaa0@gmail.com>
2018-08-31 23:21:51 +0200

    textv23213

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/zticlient'));
+app.use(express.static('./dist/angularr-test-app'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join('./dist/zticlient/index.html'));
+    res.sendFile(path.join('./dist/angularr-test-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
